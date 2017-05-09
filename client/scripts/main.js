729230731f5ab4e0b43adae653a9abf703fd0a68
aaronshappell <crumster88@gmail.com>
2017-05-09 16:52:16 +0300

    Fix index

--- client/scripts/main.js
+++ client/scripts/main.js
@@ -10,7 +10,7 @@ var init = function(){
     camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
     renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
-    document.getElementById("container").appendChild(render.domElement);
+    document.getElementById("container").appendChild(renderer.domElement);
 
     window.addEventListener("resize", resizeCanvas);
 
