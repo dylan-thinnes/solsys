bc45e87a37e4550494e47fd790c677caaf803474
dylan-thinnes <dylan.j.thinnes@gmail.com>
2017-07-19 22:30:38 +0900

    Completed ArbNum class for arbitrary precision numbers

--- client/scripts/ui.js
+++ client/scripts/ui.js
@@ -1,3 +1,105 @@
+var ArbNum = function (value, length) {
+	this.length = length === undefined ? 100 : length;
+	this.buffer = new ArrayBuffer(this.length);
+	this.reader = new Uint8Array(this.buffer);
+	this.value = value;
+}
+Object.defineProperties(ArbNum.prototype, {
+	'value': {
+		get: function () {
+			return this.reader.slice().reverse().join('');
+		},
+		set: function (newValue) {
+			if (newValue.constructor === String) {
+				var minLength = newValue.length > this.length ? this.length : newValue.length;
+				for (var ii = 0; ii < minLength; ii++) {
+					this.reader[ii] = parseInt(newValue[minLength - ii - 1]);
+				}
+			} else if (newValue.constructor === Array || newValue.constructor === Uint8Array || newValue.constructor === Uint8ClampedArray) {
+				var minLength = newValue.length > this.length ? this.length : newValue.length;
+				for (var ii = 0; ii < minLength; ii++) {
+					this.reader[ii] = parseInt(newValue[ii]);
+				}
+			}
+		}
+	}
+});
+ArbNum.prototype.setDigit = function (place, newDigit) {
+	if (place < this.length) this.reader[place] = newDigit;
+}
+ArbNum.prototype.getDigit = function (place) {
+	if (place >= this.length) return 0;
+	return this.reader[place];
+}
+ArbNum.prototype.addDigit = function (place, operand) {
+	var res = this.getDigit(place) + operand;
+	if (res > 9) {
+		this.setDigit(place, res - 10);
+		this.addDigit(place + 1, 1);
+	} else {
+		this.setDigit(place, res);
+	}
+}
+ArbNum.prototype.subtractDigit = function (place, operand) {
+	var res = this.getDigit(place) - operand;
+	if (res < 0) {
+		this.setDigit(place, 10 + res);
+		this.subtractDigit(place + 1, 1);
+	} else {
+		this.setDigit(place, res);
+	}
+}
+ArbNum.prototype.add = function (operand) {
+	if (operand.constructor !== ArbNum) var operand = new ArbNum(operand);
+	var minLength = operand.length > this.length ? this.length : operand.length;
+	for (var ii = 0; ii < minLength; ii++) {
+		this.addDigit(ii, operand.getDigit(ii));
+	}
+}
+ArbNum.prototype.subtract = function (operand) {
+	if (operand.constructor !== ArbNum) var operand = new ArbNum(operand);
+	var minLength = operand.length > this.length ? this.length : operand.length;
+	for (var ii = 0; ii < minLength; ii++) {
+		this.subtractDigit(ii, operand.getDigit(ii));
+	}
+}
+
+var Seed = function (input) {
+	this.input = input;
+	this.type = this.NUMBER;
+	var inputLength = input.length;
+	var codePoints = [];
+	var maxCodePoint = 57;
+	for (var ii = 0; ii < inputLength; ii++) {
+		codePoints[ii] = input.codePointAt(ii);
+		if (codePoints[ii] > maxCodePoint || codePoints[ii] < 48) maxCodePoint = codePoints[ii];
+	}
+	if (maxCodePoint === 57) {
+		this.result = this.input;
+	} else {
+		if (maxCodePoint < 256) {
+			this.type = this.ASCII;
+			var buffer = new ArrayBuffer(inputLength);
+			var reader = new Uint8Array(buffer);
+		} else if (maxCodePoint < 65536) {
+			this.type = this.UTF16;
+			var buffer = new ArrayBuffer(inputLength * 2);
+			var reader = new Uint16Array(buffer);
+		} else if (maxCodePoint < 4294967296) {
+			this.type = this.UTF32;
+			var buffer = new ArrayBuffer(inputLength * 4);
+			var reader = new Uint32Array(buffer);
+		}
+		for (var ii = 0; ii < inputLength; ii++) reader[ii] = codePoints[ii];
+	}
+	return reader;
+}
+Seed.prototype.NUMBER = 0;
+Seed.prototype.ASCII = 1;
+Seed.prototype.UTF16 = 2;
+Seed.prototype.UTF32 = 3;
+
+
 var Buttons = function () {
 	this.buttons = {}
 	this.groups = {}
@@ -120,7 +222,7 @@ document.body.onload = function () {
 
 	stage1.showGroup("root");
 
-	var MQ = MathQuill.getInterface(MathQuill.getInterface.MAX);
+	/*var MQ = MathQuill.getInterface(MathQuill.getInterface.MAX);
 	var seed = MQ.MathField(document.getElementById("seed"), {
 		supSubsRequireOperand: true,
 		substituteTextarea: function () {
@@ -150,6 +252,12 @@ document.body.onload = function () {
 				}
 			}
 			return sub;
+		}
+	});*/
+	document.getElementById("seed").addEventListener("keypress", function (event) {
+		if (event.keyCode === 13 || event.charCode === 13) {
+			event.preventDefault();
+			console.log(new Seed(event.target.innerHTML));
 		}
 	});
 }
