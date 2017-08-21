f5561fdba4e28e518112a0446cac94527d36f88e
aaronshappell <crumster88@gmail.com>
2017-08-21 19:42:07 +0300

    Add orbit paths

--- client/data/solar-system-icons/orbit.js
+++
@@ -1,9 +0,0 @@
-ctx = document.getElementById("planetCanvas").getContext("2d");
-ctx.clearRect(0, 0, 1000, 1000);
-ctx.save();
-ctx.strokeStyle = "#ffffff";
-ctx.fillStyle = "transparent";
-ctx.lineWidth = 1;
-ctx.arc(500, 500, 500, 0, 2 * Math.PI, false);
-ctx.stroke();
-ctx.restore();
