533deab74e7d0e9461946539d693082f94f4157e
Dylan Thinnes <dylan.j.thinnes@gmail.com>
2017-11-22 18:42:00 +0000

    Remove OrbitControls zoom dampening

--- client/scripts/OrbitControls.js
+++ client/scripts/OrbitControls.js
@@ -27,7 +27,7 @@ THREE.OrbitControls = function ( object, domElement ) {
 	this.target = new THREE.Vector3();
 
 	// How far you can dolly in and out ( PerspectiveCamera only )
-	this.minDistance = 0;
+	this.minDistance = -4000;
 	this.maxDistance = 4000;
 
 	// How far you can zoom in and out ( OrthographicCamera only )
